807954ed8f08b3de526f83ae015e2f3a89630c72
Juansebastian987 <juanse.bastian2012@hotmail.com>
2021-05-20 10:10:59 -0500

    SET games

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/books-ui'));
+app.use(express.static(__dirname + '/dist/games-ui'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(`${__dirname}/dist/books-ui/index.html`));
+res.sendFile(path.join(`${__dirname}/dist/games-ui/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'books-ui'`, () => {
+  it(`should have as title 'games-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Library');
